015a74c75acb4c78f43f9848348de976ccaf0c1d
RanjitsinhMule <muleranjitsinh7323@gmail.com>
2022-08-25 08:26:42 +0530

    Added; Printing Welcome Messege

---
+++ src/com/bridgelabz/EmpWagebuilder.java
@@ -0,0 +1,11 @@
+package com.bridgelabz;
+
+public class EmpWagebuilder {
+
+	public static void main(String[] args) {
+		totalEmpWage();
+	}
+	private static void totalEmpWage(){
+		System.out.println("Welcome to Employee Wage Computation Program ");
+	}
+}
